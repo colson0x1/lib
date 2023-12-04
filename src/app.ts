a383662acd4882d2770bf8e774112bfeafd36758
Colson <colsoncalifornication@gmail.com>
2023-12-04 19:18:57 +0545

    retrieve user input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('COLSON!');
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchLocationHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+}
+
+form.addEventListener('submit', searchLocationHandler);
