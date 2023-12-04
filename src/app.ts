779217bf3e621e9e7cee3a43166bc3345c1dd42b
Colson <colsoncalifornication@gmail.com>
2023-12-04 20:24:59 +0545

    render maps

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
-const GOOGLE_API_KEY = '';
+const GOOGLE_API_KEY = 'API_KEY';
 
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
   status: 'OK' | 'ZERO_RESULTS';
 };
+
+// declare var google: any;
 
 function searchLocationHandler(event: Event) {
   event.preventDefault();
@@ -17,13 +19,19 @@ function searchLocationHandler(event: Event) {
     .get<GoogleGeocodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress,
-      )}&key=${GOOGLE_API_KEY}&callback=initMap`,
+      )}&key=${GOOGLE_API_KEY}`,
     )
     .then((response) => {
       if (response.data.status !== 'OK') {
         throw new Error('Could not fetch location!');
       }
       const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: coordinates,
+        zoom: 16,
+      });
+
+      new google.maps.Marker({ position: coordinates, map: map });
     })
     .catch((err) => {
       alert(err.message);
